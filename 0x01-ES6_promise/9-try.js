4e7c9d2047e620a0b2c942d5eb876b0538f2556b
trillionclues <trillionclues@gmail.com>
2022-12-20 17:08:47 +0100

    ES6 Promise

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+export default function guardrail(mathFunction) {
+  const array = [];
+
+
+  try {
+    array.push(mathFunction());
+  } catch (err) {
+    array.push(err.toString());
+  } finally {
+    array.push('Guardrail was processed');
+  }
+  return array;
+}
